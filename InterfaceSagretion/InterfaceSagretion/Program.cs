320a7ffe113e0377a227f1153feacb7d3e27a944
JavidGasim <143398895+JavidGasim@users.noreply.github.com>
2023-12-03 17:00:00 +0400

    Add files via upload

---
+++ InterfaceSagretion/InterfaceSagretion/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace InterfaceSagretion;
+
+interface IWorkerGood
+{
+    void Work();
+}
+
+class GoodWorker : IWorkerGood
+{
+    public void Work()
+    {
+        Console.WriteLine("Working");
+    }
+}
+
+interface IEater
+{
+    void Eat();
+}
+
+interface ISleeper
+{
+    void Sleep();
+}
+
+class BetterWorker : IWorkerGood, IEater, ISleeper
+{
+    public void Work()
+    {
+        Console.WriteLine("Working");
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine("Eating");
+    }
+
+    public void Sleep()
+    {
+        Console.WriteLine("Sleeping");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IWorkerGood goodWorker = new GoodWorker();
+        goodWorker.Work();
+
+        Console.WriteLine();
+
+        BetterWorker betterWorker = new BetterWorker();
+        betterWorker.Work();
+        betterWorker.Eat();
+        betterWorker.Sleep();
+    }
+}
